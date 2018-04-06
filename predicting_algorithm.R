e15386eb67cdfb1e0407a9a4af87bd5a14e76191
Daniel <daniel.jungen@t-online.de>
2018-04-06 20:26:27 +0200

    updates

--- predicting_algorithm.R
+++ predicting_algorithm.R
@@ -1,4 +1,4 @@
-setwd("C:/Coursera/Capstone Project")
+setwd("D:/Coursera/Capstone Project")
 
 
 freq2_gr1<-readRDS("freq2_gr1.rds")
@@ -9,46 +9,116 @@ freq6_gr1<-readRDS("freq6_gr1.rds")
 
 library(dplyr)
 
-phrase<-"that is a test with many words for testing bla bla bla another test"
-
-test<-strsplit(phrase, " ")
-
-length_test<-length(test[[1]])-1
-
-if(length_test >5){
-  
-    test[[1]][-1:-(length(test[[1]])-5)]
-  
-  }
-
-if(length_test == 5){
-    
-  predictor <- freq6_gr1[freq6_gr1$input == test]
-  
-}
-
-if(length_test == 4){
-  
-  predictor <- freq5_gr1[freq5_gr1$input == test]
-  
-}
-
-
-if(length_test == 3){
-  
-  predictor <- freq4_gr1[freq4_gr1$input == test]
-  
-}
-
-
-if(length_test == 2){
-  
-  predictor <- freq3_gr1[freq3_gr1$input == test]
-  
-}
-
-if(length_test == 1){
-  
-  predictor <- freq2_gr1[freq2_gr1$input == test]
-  
+predict <- function(phrase){
+        
+        original_phrase<- phrase
+        
+        phrase<- tolower(phrase)
+        phrase <- gsub("'", '', phrase)
+        
+        
+        
+        phrase_split<-strsplit(phrase, " ")
+        
+        length_phrase<-length(phrase_split[[1]])
+        
+        if(length_phrase >5){
+                
+                phrase_split<-phrase_split[[1]][-1:-(length(phrase_split[[1]])-5)]
+                
+                phrase<-paste(phrase_split[[1]],phrase_split[[2]],phrase_split[[3]],phrase_split[[4]],phrase_split[[5]])
+                
+        }
+        
+        if(length_phrase == 5){
+                
+                predictor <- freq6_gr1[freq6_gr1$input == phrase]
+                
+                if(dim(predictor)[1] >0 & dim(predictor)[2]>0){
+                        predictor <- predictor[order(-predictor$frequency),]
+                        predicted_word<-predictor[1,2]
+                }
+                
+                if(dim(predictor)[1]==0 || dim(predictor)[2]==0){
+                        phrase_split<-strsplit(phrase, " ")
+                        phrase_split<-phrase_split[[1]][-1:-(length(phrase_split[[1]])-4)]
+                        phrase<-paste(phrase_split[[1]],phrase_split[[2]],phrase_split[[3]],phrase_split[[4]])
+                        length_phrase<-4
+                }
+                
+        }
+        
+        if(length_phrase == 4){
+                
+                predictor <- freq5_gr1[freq5_gr1$input == phrase]
+                
+                if(dim(predictor)[1] >0 & dim(predictor)[2]>0){
+                        predictor <- predictor[order(-predictor$frequency),]
+                        predicted_word<-predictor[1,2]
+                }
+                
+                if(dim(predictor)[1]==0 || dim(predictor)[2]==0){
+                        phrase_split<-strsplit(phrase, " ")
+                        phrase_split<-phrase_split[[1]][-1:-(length(phrase_split[[1]])-3)]
+                        phrase<-paste(phrase_split[[1]],phrase_split[[2]],phrase_split[[3]])
+                        length_phrase<-3
+                }
+                
+        }
+        
+        
+        if(length_phrase == 3){
+                
+                predictor <- freq4_gr1[freq4_gr1$input == phrase,]
+                
+                if(dim(predictor)[1] >0 & dim(predictor)[2]>0){
+                        predictor <- predictor[order(-predictor$frequency),]
+                        predicted_word<-predictor[1,2]
+                }
+                
+                if(dim(predictor)[1]==0 || dim(predictor)[2]==0){
+                        phrase_split<-strsplit(phrase, " ")
+                        phrase_split<-phrase_split[[1]][-1:-(length(phrase_split[[1]])-2)]
+                        phrase<-paste(phrase_split[[1]],phrase_split[[2]])
+                        length_phrase<-2
+                }
+                
+        }
+        
+        
+        if(length_phrase == 2){
+                
+                predictor <- freq3_gr1[freq3_gr1$input == phrase,]
+                
+                if(dim(predictor)[1] >0 & dim(predictor)[2]>0){
+                        predictor <- predictor[order(-predictor$frequency),]
+                        predicted_word<-predictor[1,2]
+                }
+                
+                if(dim(predictor)[1]==0 || dim(predictor)[2]==0){
+                        phrase_split<-strsplit(phrase, " ")
+                        phrase<-phrase_split[[1]][-1:-(length(phrase_split[[1]])-1)]
+                        length_phrase<-1
+                }
+                
+        }
+        
+        if(length_phrase == 1){
+                
+                predictor <- freq2_gr1[freq2_gr1$input == phrase,]
+                
+                if(dim(predictor)[1] >0 & dim(predictor)[2]>0){
+                        predictor <- predictor[order(-predictor$frequency),]
+                        predicted_word<-predictor[1,2]
+                }
+                
+                if(dim(predictor)[2]==0){
+                        predicted_word<-"you" #standard prediction if no better one can be determined.
+                        #this is the most frequent single word used.
+                }
+                
+        }
+        
+        return(predicted_word)
+        
 }
