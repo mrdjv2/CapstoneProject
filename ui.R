1ae12e3dd6f36c8a1dbfd3b8d5663e1345c57cae
Daniel <daniel.jungen@t-online.de>
2018-04-07 17:43:40 +0200

    update

--- ui.R
+++ ui.R
@@ -5,13 +5,13 @@ fluidPage(
   
   mainPanel(
     h3("Introduction:"),
-    h5("This application takes your string and predict the next world"),
+    h5("This application predicts the next word based on your entered string"),
     h3("Method:"),
-    h5("Use MLE of n-gram algorithm"),
+    h5("Frequency analysis of n-grams"),
     
-    textInput("Tcir",label=h3("Type your sentence here:")),
+    textInput("Tcir",label=h3("Type in your phrase:")),
     submitButton('Submit'),
-    h4('string you entered : '),
+    h4('entered phrase: '),
     verbatimTextOutput("inputValue"),
     h4('next word :'),
     verbatimTextOutput("prediction")
